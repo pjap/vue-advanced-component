0998056e3ebeb48bc177ad090d1a78a0f7382eff
pjap <pranajaya.pratama@gmail.com>
2018-07-30 11:45:11 +0700

    How To Use Dynamic Components Using Slot

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <br>
+        <button class="btn btn-warning" @click="componentSelector = 'appQuote'">Quote</button>
+        <button class="btn btn-info" @click="componentSelector = 'appAuthor'">Author</button>
+        <button class="btn btn-success" @click="componentSelector = 'appNew'">New Quote</button>
+        <span> You Are Here --> <strong>{{ componentSelector }}</strong></span>
+        <component :is="componentSelector">
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p>Default Content</p>
+        </component>
+        <!-- If Using Slot All The Parent Data Will Render In Child Except Style -->
+        <!-- <appQuote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot>A Wonderful Quote</p>
+        </appQuote> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue'
+import Author from './components/Author.vue'
+import Newcomp from './components/New.vue'
+
+export default {
+  data: function() {
+    return {
+      quoteTitle: 'The Quote',
+      componentSelector: 'appQuote'
+    }
+  },
+  components: {
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: Newcomp
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="title">
+      <!-- Using Slot For Pass Argument HTML From Outside To Child Components -->
+      <slot name="title"></slot>
+      <span style="color:black;font-size:12px;"><slot name="subtitle">The Subtitle In Here</slot></span>
+    </div>
+    <hr>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  div {
+    /* border: 1px solid #ccc; */
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+  h2 {
+    color: tomato;
+  }
+  .title {
+    font-style: italic;
+    text-transform: uppercase;
+  }
+</style>
